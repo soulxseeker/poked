236d4413e14a37544317727470a0b60951830227
Softsquare <Soulxsense@soulxsenses-macbook-pro.local>
2016-06-20 21:15:35 +0700

    Initial Commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Soulxsense on 6/20/2559 BE.
+//  Copyright © 2559 Soulxsense. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
